b6511004c542109ab273b9e23a8c54d49ca3d65c
NexusDevelopmentUA <unnexusgcm@gmail.com>
2016-11-29 12:56:43 +0200

    Fist task done. Start working on second one.

---
+++ AcademyTask/Models/Friends.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AcademyTask.Models
+{
+    public class Friends
+    {
+        public int Id { get; set; }
+        public ApplicationUser First_user { get; set; }
+        public ApplicationUser Second_user { get; set; }
+        public string Relationships { get; set; }
+    }
+    public class FriendContext:DbContext
+        {
+            public FriendContext():base("DefaultConnection")
+            {
+
+            }
+            public DbSet<Friends> Friends { get; set; }
+        }
+}
