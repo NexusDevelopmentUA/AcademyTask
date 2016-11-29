37c04de33ebba258769c8929bbd14805d1550612
NexusDevelopmentUA <unnexusgcm@gmail.com>
2016-11-29 12:56:43 +0200

    Fist task done. Start working on second one.

---
+++ AcademyTask/Models/Friends.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcademyTask.Models
+{
+    public class Friends
+    {
+        public int Id { get; set; }
+        public ApplicationUser First_user { get; set; }
+        public ApplicationUser Second_user { get; set; }
+        public string Relationships { get; set; }
+    }
+}
