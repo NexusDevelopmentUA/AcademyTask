83876fbf04aade329c2c866f003e42eaf5f20232
NexusDevelopmentUA <unnexusgcm@gmail.com>
2016-11-29 13:37:13 +0200

    Revert "Fist task done. Start working on second one."
    This reverts commit b6511004c542109ab273b9e23a8c54d49ca3d65c.

--- AcademyTask/Controllers/HomeController.cs
+++ AcademyTask/Controllers/HomeController.cs
@@ -2,22 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using AcademyTask.Models;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
 
 namespace AcademyTask.Controllers
 {
     public class HomeController : Controller
     {
-        ApplicationDbContext context = new ApplicationDbContext();
-
         public ActionResult Index()
         {
-            var CurrentUserId = User.Identity.GetUserId();
-            IEnumerable<ApplicationUser> users = context.Users;
-            ViewBag.CurrentUserId = CurrentUserId;
-            ViewBag.Users = users;
             return View();
         }
 
